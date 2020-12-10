5bfc77fc3fa12e1604ba1ebe90762417b16c6f21
SamRushan <31480879+SamRushan@users.noreply.github.com>
2020-12-10 21:41:11 +0100

    Update PetClinicApplication.java
    @AutoConfigureTestDatabase(replace = NONE)

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -26,6 +26,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication(proxyBeanMethods = false)
+@AutoConfigureTestDatabase(replace = NONE)
 public class PetClinicApplication {
 
 	public static void main(String[] args) {
